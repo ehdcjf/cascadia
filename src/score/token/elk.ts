b9c773a56d2aea8dfd816dd69f78f6c89040bb63
ehdckf <ehdcjf@funnyfig.com>
2024-04-25 12:58:38 +0900

    elk 작업중

--- src/score/token/elk.ts
+++ src/score/token/elk.ts
@@ -1,3 +1,5 @@
+import { getRowColMapIndex, idToRowColumn } from "../../utils";
+import { directions, linkedTileSides } from "../const";
 import { allPlacedTokens } from "../tile";
 import { searchNeighbourTilesForWildlife } from "./util";
 
@@ -9,16 +11,18 @@ const ElkScoringValue: Record<number, number> = {
 };
 
 export class ElkScoring {
-        private score = 0;
-        private allElkTokens: string[] = [];
-        private usedElkTokenIDs: string[] = [];
-        private potentialElkLines: string[] = [];
-        private confirmedElkLines: any[][] = [];
-        private potentialElkLineStartingTokens = {
+        protected score = 0;
+        protected allElkTokens: string[] = [];
+        protected usedElkTokenIDs: string[] = [];
+        protected potentialElkLines: string[][] = [];
+        protected confirmedElkLines: any[][] = [];
+        protected potentialElkLineStartingTokens: { E: any[]; SE: any[]; SW: any[] } = {
                 E: [],
                 SE: [],
                 SW: [],
         };
+        protected allLineDetails: any = {};
+        protected sharedElkTokenIDs: any = {};
         constructor() {}
 
         calculateElkTokenScoring() {
@@ -59,6 +63,123 @@ export class ElkScoring {
                         }
                 }
 
-                for (let i = 0; i < this.allElkTokens.length; i++) {}
+                this.allElkTokens.forEach((elkToken) => {
+                        const validStartingLineDirection = this.checkVaildStartingElkToken(elkToken);
+                        if (validStartingLineDirection && validStartingLineDirection.length > 0) {
+                                for (let j = 0; j < validStartingLineDirection.length; j++) {
+                                        this.potentialElkLineStartingTokens[validStartingLineDirection[j]].push(
+                                                elkToken,
+                                        );
+                                }
+                        }
+                });
+
+                Object.entries(this.potentialElkLineStartingTokens).forEach(([direction, value]) => {
+                        value.forEach((token) => {
+                                const currentPotentialElkLine = this.allElkTokensInLine(token, direction);
+                                this.potentialElkLines.push(currentPotentialElkLine);
+                        });
+                });
+
+                this.potentialElkLines.sort((a, b) => b.length - a.length);
+
+                const checkedIndexCombos = [];
+                const allSharedElkTokenLineNums = [];
+
+                for (let i = this.potentialElkLines.length - 1; i >= 0; i--) {}
+        }
+
+        protected nextElkTokenInDirection(tileID: string, direction: string) {
+                const [row, column] = idToRowColumn(tileID);
+                const rowColMapIndex = getRowColMapIndex(row);
+                const directionIndex = directions.indexOf(direction);
+                const newRow = row + linkedTileSides[directionIndex].rowColMapping[rowColMapIndex].rowDiff;
+                const newColumn = column + linkedTileSides[directionIndex].rowColMapping[rowColMapIndex].columnDiff;
+                const newTileID = `${newRow}-${newColumn}`;
+
+                if (!allPlacedTokens.hasOwnProperty(newTileID)) return false;
+                if (allPlacedTokens[newTileID] != "elk") return false;
+                if (this.usedElkTokenIDs.includes(newTileID)) return false;
+                return newTileID;
+        }
+
+        protected allElkTokensInLine(tileID: string, direction: string) {
+                const matchedLineIDs = [tileID];
+                let lastTokenID = tileID;
+                let elkLineLimit = 0;
+                while (true) {
+                        const nextTokenID = this.nextElkTokenInDirection(lastTokenID, direction);
+                        if (nextTokenID) {
+                                matchedLineIDs.push(nextTokenID);
+                                elkLineLimit++;
+                                lastTokenID = nextTokenID;
+                        } else {
+                                return matchedLineIDs;
+                        }
+                }
+        }
+
+        protected checkVaildStartingElkToken(tileID: string): false | ("E" | "SE" | "SW")[] {
+                const blankSides = [0, 4, 5];
+                const lineCheckSides = [3, 1, 2];
+
+                const validElkLineDirections: string[] = [];
+
+                const [row, column] = idToRowColumn(tileID);
+                const rowColMapIndex = getRowColMapIndex(row);
+
+                [
+                        [0, 3],
+                        [4, 1],
+                        [5, 2],
+                ].forEach(([blank, lineCheckSide]) => {
+                        const oppositeDirection = linkedTileSides[lineCheckSide].direction;
+
+                        const precedingRow = row + linkedTileSides[blank].rowColMapping[rowColMapIndex].rowDiff;
+                        const precedingColumn =
+                                column + linkedTileSides[blank].rowColMapping[rowColMapIndex].columnDiff;
+                        const precedingTileID = precedingRow + precedingColumn;
+
+                        const followingRow = row + linkedTileSides[lineCheckSide].rowColMapping[rowColMapIndex].rowDiff;
+                        const followingColumn =
+                                column + linkedTileSides[lineCheckSide].rowColMapping[rowColMapIndex].columnDiff;
+                        const followingTileID = followingRow + followingColumn;
+
+                        let validPrecedingTile = false;
+                        let validFollowingTile = false;
+
+                        if (allPlacedTokens.hasOwnProperty(precedingTileID)) {
+                                if (allPlacedTokens[precedingTileID] != "elk") {
+                                        validPrecedingTile = true;
+                                }
+                        } else {
+                                validPrecedingTile = true;
+                        }
+
+                        if (allPlacedTokens.hasOwnProperty(followingTileID)) {
+                                if (allPlacedTokens[followingTileID] == "elk") {
+                                        validFollowingTile = true;
+                                }
+                        }
+
+                        if (validFollowingTile && validPrecedingTile) {
+                                validElkLineDirections.push(oppositeDirection);
+                        }
+                });
+
+                return validElkLineDirections.length == 0 ? false : (validElkLineDirections as ("E" | "SE" | "SW")[]);
+        }
+
+        protected processStanaloneElkLine(elkLine: string[]) {
+                if (elkLine.length <= 4) {
+                        this.confirmedElkLines.push(elkLine);
+                        this.usedElkTokenIDs.push(...elkLine);
+                } else {
+                        for (let i = 0; i < elkLine.length; i += 4) {
+                                const tempArray = elkLine.slice(i, i + 4);
+                                this.confirmedElkLines.push(tempArray);
+                                this.usedElkTokenIDs.push(...tempArray);
+                        }
+                }
         }
 }
