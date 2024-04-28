d214badb7f9d1e17100b2be0eb3a0026d9b056e2
ehdckf <ehdcjf@funnyfig.com>
2024-04-29 01:00:12 +0900

    hexagon

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,123 @@
-import "./styles.css";
-import { startingTiles, tiles } from "./data";
-import { Tile, WildLife } from "./interfaces";
-const turnsLeft = 20;
-const wildlife = ["bear", "fox", "elk", "hawk", "samlon"];
-const tokenNums: Record<WildLife, number> = {
-        bear: 20,
-        salmon: 20,
-        hawk: 20,
-        elk: 20,
-        fox: 20,
-};
+import "@babylonjs/core/Debug/debugLayer";
+import { Inspector } from "@babylonjs/inspector";
+import "@babylonjs/loaders/glTF";
 
-const allTiles: any[] = [];
-const allTokens: any[] = [];
-const initialTiles: any[] = [];
-const initialTokens: any[] = [];
-const dispalyedTokens: any[] = [];
-const mapData: any[] = [];
+import {
+  Engine,
+  Scene,
+  Vector3,
+  FreeCamera,
+  SceneLoader,
+  EngineFactory,
+  HemisphericLight,
+  ArcRotateCamera,
+  Tools,
+  AbstractMesh,
+  Color3,
+} from "@babylonjs/core";
 
-const mapRowsColumnsIndexes = {
-        rows: {},
-        column: {},
-};
+import * as BABYLON from "@babylonjs/core";
+import { createHexagons } from "./cube";
+(window as any).BABYLON = BABYLON;
 
-const mapLimits = { up: 10, down: 30, left: 10, right: 30 };
+const H = 1.5;
+const W = Math.cos(Math.PI / 6);
 
-const mapMoveAmount = {
-        tilePos: {
-                top: 0,
-                left: 0,
-        },
-        view: {
-                desktop: {
-                        incs: {
-                                vertical: 85,
-                                horizontal: 100,
-                        },
-                        unit: "px",
-                },
-        },
-};
+class App {
+  private scene!: Scene;
+  private engine!: Engine;
 
-const mapRowRange = mapLimits.down - mapLimits.up; // 22
-const mapColumnRange = mapLimits.right - mapLimits.left; //22
+  constructor() {
+    this.init();
+  }
 
-class Board {}
+  private async init() {
+    const canvas = document.querySelector("#gameCanvas") as HTMLCanvasElement;
+    this.engine = (await EngineFactory.CreateAsync(
+      canvas,
+      undefined
+    )) as Engine;
+
+    this.createScene();
+    this.engine.runRenderLoop(() => {
+      if (this.scene) this.scene.render();
+    });
+    window.addEventListener("resize", () => {
+      this.engine.resize();
+    });
+  }
+
+  private async createScene() {
+    this.engine.displayLoadingUI();
+    this.scene = new Scene(this.engine);
+    const camera = new ArcRotateCamera(
+      "camera",
+      Tools.ToRadians(90),
+      Tools.ToRadians(45),
+      10,
+      Vector3.Zero()
+    );
+    camera.attachControl(true);
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0));
+
+    Inspector.Show(this.scene, {});
+
+    await this.loadAssetAsync();
+    await this.scene.whenReadyAsync();
+    this.engine.hideLoadingUI();
+  }
+
+  //   private setCamera() {
+  //     const camera = new FreeCamera("camera", new Vector3(0, 6, 20));
+  //     camera.setTarget(Vector3.Zero());
+  //     camera.inputs.addMouseWheel();
+  //     camera.attachControl(true);
+  //   }
+
+  //   private setLight() {
+  //     const light = new HemisphericLight("light", Vector3.Zero());
+  //     light.intensity = 0.7;
+  //   }
+
+  private async loadAssetAsync() {
+    const tiles = await SceneLoader.ImportMeshAsync(
+      "",
+      "./models/",
+      "cascadia.glb"
+    );
+
+    const meshMap: Record<string, AbstractMesh> = {};
+    tiles.meshes.forEach((mesh) => {
+      meshMap[mesh.id] = mesh;
+    });
+
+    //     tiles.meshes.forEach((v) => {});
+
+    //     const meshMap: Map<string, AbstractMesh> = new Map();
+    for (let i = 1; i < tiles.meshes.length; i++) {
+      const mesh = tiles.meshes[i];
+      mesh.visibility = 0;
+    }
+
+    const beige = meshMap["begie"];
+
+    const hexSet = createHexagons(1);
+
+    hexSet.forEach((cube) => {
+      const [column, row] = cube.cr;
+      const [q, r, s] = cube.qrs;
+      const oddOffset = row % 2 == 0 ? 0 : W;
+      const x = oddOffset + 2 * W * column;
+      const z = -row * H;
+      const key = `${q}/${r}/${s}`;
+      const boardMesh = beige.clone(key, beige.parent)!;
+      boardMesh.visibility = 1;
+      boardMesh.position = new Vector3(x, 0, z);
+      boardMesh.renderOutline = true;
+      boardMesh.outlineColor = new Color3(0, 0, 0);
+      boardMesh.outlineWidth = 0;
+    });
+  }
+}
+
+new App();
