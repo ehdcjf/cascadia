1db5764d8fb10d9922d8607565c0b71a92b94e0c
ehdckf <ehdcjf@funnyfig.com>
2024-05-02 00:37:55 +0900

    연어 계산 수정

--- src3/score/token/salmon.ts
+++ src3/score/token/salmon.ts
@@ -1,125 +1,154 @@
-import { MapItem } from "../../board";
-import { Queue } from "../../utils";
+import { MapItem } from '../../board';
+import { Queue } from '../../utils';
 const SalmonScoringValue: Record<number, number> = {
-        1: 2,
-        2: 4,
-        3: 7,
-        4: 11,
-        5: 15,
-        6: 20,
-        7: 26,
+	1: 2,
+	2: 4,
+	3: 7,
+	4: 11,
+	5: 15,
+	6: 20,
+	7: 26,
 };
 
 export class SalmonScoring {
-        //  이미 사용했거나 조건 충족하지 않은 연어 목록
-        private usedToken: Set<string> = new Set();
-        private confirmedSalmonRuns: Array<Set<string>> = [];
-        private validSalmonTokens: Array<string> = [];
-        private totalScore = 0;
-        constructor(private mapData: Map<string, MapItem>) {
-                this.getValidSalmonTokens();
-        }
+	//  이미 사용했거나 조건 충족하지 않은 연어 목록
+	private usedToken: Set<string> = new Set();
+	private confirmedSalmonRuns: Array<Set<string>> = [];
+	private totalScore = 0;
+	constructor(private mapData: Map<string, MapItem>) {
+		this.calculateSalmonToken();
+	}
 
-        private getValidSalmonTokens() {
-                for (const [key, mapItem] of this.mapData) {
-                        if (mapItem.placedToken != "salmon") continue;
+	private searchNeighborSalmonKeys(tokenKey: string) {
+		const neighborSalmons: string[] = [];
 
-                        const neighborSalmons = this.searchNeighborSalmon(key);
+		const mapItem = this.mapData.get(tokenKey)!;
+		const neighborTileKeys = mapItem.coor.neighborKeys;
+		for (const neighborTileKey of neighborTileKeys) {
+			if (!this.mapData.has(neighborTileKey)) continue;
+			const neighborTile = this.mapData.get(neighborTileKey)!;
+			if (neighborTile.placedToken === 'salmon') neighborSalmons.push(neighborTileKey);
+		}
+		return neighborSalmons;
+	}
 
-                        if (neighborSalmons.length <= 2) {
-                                this.validSalmonTokens.push(key);
-                        } else {
-                                this.usedToken.add(key);
-                        }
-                }
-        }
+	private calculateSalmonToken() {
+		const validSalmonTokens: Array<string> = [];
 
-        private searchNeighborSalmon(tokenKey: string) {
-                const neighborSalmons: string[] = [];
+		for (const [key, mapItem] of this.mapData) {
+			if (mapItem.placedToken != 'salmon') continue;
 
-                const mapItem = this.mapData.get(tokenKey)!;
-                const neighborTileKeys = mapItem.coor.neighborKeys;
-                for (const neighborTileKey of neighborTileKeys) {
-                        if (!this.mapData.has(neighborTileKey)) continue;
-                        const neighborTile = this.mapData.get(neighborTileKey)!;
-                        if (neighborTile.placedToken === "salmon") neighborSalmons.push(neighborTileKey);
-                }
-                return neighborSalmons;
-        }
+			const neighborSalmons = this.searchNeighborSalmonKeys(key);
 
-        private calculateSalmonToken() {
-                for (const mainSalmonKey of this.validSalmonTokens) {
-                        const potentialSalmonTokenIDs: Set<string> = new Set();
-                        if (this.usedToken.has(mainSalmonKey)) continue;
+			if (neighborSalmons.length <= 2) {
+				validSalmonTokens.push(key);
+			} else {
+				this.usedToken.add(key);
+			}
+		}
 
-                        const confirmedNeighborSalmons = this.searchNeighborSalmon(mainSalmonKey).filter(
-                                (neighbor) => !this.usedToken.has(neighbor),
-                        );
+		for (const mainSalmonKey of validSalmonTokens) {
+			const potentialSalmonTokenIDs: Set<string> = new Set();
+			if (this.usedToken.has(mainSalmonKey)) continue;
 
-                        if (confirmedNeighborSalmons.length == 2) {
-                                const [firstNeighborKey, secondNeighborKey] = confirmedNeighborSalmons;
+			const confirmedNeighborSalmons = this.searchNeighborSalmonKeys(mainSalmonKey).filter(
+				(neighbor) => !this.usedToken.has(neighbor)
+			);
 
-                                const firstNeighborhood = this.mapData.get(firstNeighborKey)!.coor.neighborKeys;
-                                const secondNeighborhood = this.mapData.get(secondNeighborKey)!.coor.neighborKeys;
+			if (confirmedNeighborSalmons.length == 2) {
+				const [firstNeighborKey, secondNeighborKey] = confirmedNeighborSalmons;
 
-                                if (
-                                        !firstNeighborhood.includes(secondNeighborKey) &&
-                                        !secondNeighborhood.includes(firstNeighborKey)
-                                ) {
-                                        const forwardsAndBackwardsSalmonRunIDs = this.forwardsAndBackwardsSalmonRun();
-                                } else {
-                                        [mainSalmonKey, firstNeighborKey, secondNeighborKey].forEach((tokenKey) => {
-                                                potentialSalmonTokenIDs.add(tokenKey);
-                                                this.usedToken.add(tokenKey);
-                                        });
-                                }
-                        } else if (confirmedNeighborSalmons.length < 2) {
-                                potentialSalmonTokenIDs.add(mainSalmonKey);
-                                const salmonRunTokenIDs = this.salmonTokensInRun(mainSalmonKey);
-                                salmonRunTokenIDs.forEach((key) => {
-                                        potentialSalmonTokenIDs.add(key);
-                                });
-                        }
-                        this.confirmedSalmonRuns.push(potentialSalmonTokenIDs);
-                }
+				// 하나만 체크해도 될듯?
+				const firstNeighborhood = this.mapData.get(firstNeighborKey)!.coor.neighborKeys;
+				const secondNeighborhood = this.mapData.get(secondNeighborKey)!.coor.neighborKeys;
 
-                this.confirmedSalmonRuns.forEach((salmonIDs) => {
-                        const salomInRunNums = salmonIDs.size > 7 ? 7 : salmonIDs.size;
-                        this.totalScore += SalmonScoringValue[salomInRunNums];
-                });
-        }
+				if (
+					!firstNeighborhood.includes(secondNeighborKey) &&
+					!secondNeighborhood.includes(firstNeighborKey)
+				) {
+					const forwardsAndBackwardsSalmonRunIDs = this.forwardsAndBackwardsSalmonRun(
+						mainSalmonKey,
+						confirmedNeighborSalmons
+					);
+					forwardsAndBackwardsSalmonRunIDs.forEach((key) => {
+						potentialSalmonTokenIDs.add(key);
+					});
+					potentialSalmonTokenIDs;
+				} else {
+					[mainSalmonKey, firstNeighborKey, secondNeighborKey].forEach((tokenKey) => {
+						potentialSalmonTokenIDs.add(tokenKey);
+						this.usedToken.add(tokenKey);
+					});
+				}
+			} else if (confirmedNeighborSalmons.length < 2) {
+				potentialSalmonTokenIDs.add(mainSalmonKey);
+				const salmonRunTokenIDs = this.salmonTokensInRun(mainSalmonKey);
+				salmonRunTokenIDs.forEach((key) => {
+					potentialSalmonTokenIDs.add(key);
+				});
+			}
+			this.confirmedSalmonRuns.push(potentialSalmonTokenIDs);
+		}
 
-        private salmonTokensInRun(startKey: string) {
-                const salmonRunIDs = [];
-                let nextTokenKey = startKey;
+		this.confirmedSalmonRuns.forEach((salmonIDs) => {
+			const salomInRunNums = salmonIDs.size > 7 ? 7 : salmonIDs.size;
+			this.totalScore += SalmonScoringValue[salomInRunNums];
+		});
+	}
 
-                salmonRunIDs.push(startKey);
-                this.usedToken.add(startKey);
+	private salmonTokensInRun(startKey: string) {
+		const salmonRunIDs = [];
+		const q = new Queue();
 
-                let salmonRunEnded = false;
+		salmonRunIDs.push(startKey);
+		this.usedToken.add(startKey);
+		q.push(startKey);
 
-                while (!salmonRunEnded) {
-                        const neighborSalmons = this.searchNeighborSalmon(nextTokenKey).filter(
-                                (neighbor) => !this.usedToken.has(neighbor),
-                        );
+		while (q.size > 0) {
+			const key = q.pop();
+			const neighborSalmons = this.searchNeighborSalmonKeys(key).filter(
+				(neighbor) => !this.usedToken.has(neighbor)
+			);
 
-                        if (neighborSalmons.length == 1) {
-                                const nextSalmon = neighborSalmons[0];
-                                salmonRunIDs.push(nextSalmon);
-                                this.usedToken.add(nextSalmon);
-                                nextTokenKey = nextSalmon;
-                        } else {
-                                salmonRunEnded = true;
-                        }
-                }
-                return salmonRunIDs;
-        }
+			if (neighborSalmons.length == 1) {
+				const nextSalmon = neighborSalmons[0];
+				salmonRunIDs.push(nextSalmon);
+				this.usedToken.add(nextSalmon);
+				q.push(neighborSalmons);
+			}
+		}
+		return salmonRunIDs;
+	}
 
-        private forwardsAndBackwardsSalmonRun() {
-                return [];
-        }
+	private forwardsAndBackwardsSalmonRun(mainTokenKey: string, neighborTokens: string[]) {
+		const forwardsAndBackwardsSalmonRunIDs: Set<string> = new Set();
+		forwardsAndBackwardsSalmonRunIDs.add(mainTokenKey);
+		this.usedToken.add(mainTokenKey);
 
-        get score() {
-                return this.totalScore;
-        }
+		neighborTokens.forEach((tokenKey) => {
+			this.usedToken.add(tokenKey);
+			const q = new Queue();
+			q.push(tokenKey);
+			while (q.size > 0) {
+				const salmonKey = q.pop();
+
+				const neighborSalmons = this.searchNeighborSalmonKeys(salmonKey).filter(
+					(neighbor) => !this.usedToken.has(neighbor)
+				);
+
+				if (neighborSalmons.length == 1) {
+					const neighbor = neighborSalmons[0];
+					forwardsAndBackwardsSalmonRunIDs.add(neighbor);
+					this.usedToken.add(neighbor);
+					q.push(neighbor);
+				}
+			}
+		});
+
+		return forwardsAndBackwardsSalmonRunIDs;
+	}
+
+	get score() {
+		return this.totalScore;
+	}
 }
