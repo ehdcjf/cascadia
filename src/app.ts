4e15711b84ed2cc5dfa45f355921142740ecabd9
ehdcjf <kdch0823@naver.com>
2024-05-17 01:51:35 +0900

    app.ts 정리

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,29 @@
 import '@babylonjs/core/Debug/debugLayer';
 import '@babylonjs/loaders/glTF';
+import { Inspector } from '@babylonjs/inspector';
 import {
-	Engine,
-	Vector3,
-	SceneLoader,
-	EngineFactory,
-	HemisphericLight,
+	ActionManager,
 	ArcRotateCamera,
-	Tools,
-	Viewport,
+	Engine,
+	EngineFactory,
+	ExecuteCodeAction,
+	HemisphericLight,
 	PointerEventTypes,
-	Observable,
+	Scene,
+	SceneLoader,
+	Tools,
+	Vector3,
+	Viewport,
 } from '@babylonjs/core';
 
-import { Scene } from './scene';
-import { Board } from './board';
-import { Pocket } from './pocket';
-import { CascadiaActionManager } from './action';
-import { Inspector } from '@babylonjs/inspector';
-import { Assets } from './assets';
-import { ScenMatadata } from './metadata';
-import { Modal } from './modal';
-// (window as any).BABYLON = BABYLON;
-
-// const H = 1.5;
-// const W = Math.cos(Math.PI / 6);
-
 class App {
-	private scene!: Scene;
 	private engine!: Engine;
-	private board!: Board;
-	private pocket!: Pocket;
-	private action!: CascadiaActionManager;
-	private modal!: Modal;
-	private observer = new Observable();
+	private scene!: Scene;
+	private inputMap: Record<string, boolean> = {};
+
 	constructor() {
 		this.init();
+		Inspector.Show(this.scene, {});
 	}
 
 	private async init() {
@@ -43,8 +31,6 @@ class App {
 		this.engine = (await EngineFactory.CreateAsync(canvas, undefined)) as Engine;
 
 		await this.createScene();
-
-		this.action = new CascadiaActionManager(this.scene);
 
 		this.engine.runRenderLoop(() => {
 			if (this.scene) this.scene.render();
@@ -57,21 +43,37 @@ class App {
 	private async createScene() {
 		this.engine.displayLoadingUI();
 		this.scene = new Scene(this.engine);
+		this.setupCamera();
+		this.setupLight();
+		await this.loadAssetAsync();
+	}
+	async loadAssetAsync() {
+		const assets = await SceneLoader.ImportMeshAsync('', './models/', 'cascadia-final.glb', this.scene);
+		assets.meshes.forEach((mesh) => {
+			mesh.setEnabled(false);
+		});
+	}
 
-		this.scene.metadata = new ScenMatadata();
-		const camera = new ArcRotateCamera(
-			'camera',
+	private setupCamera() {
+		const boardCam = new ArcRotateCamera(
+			'board-cam',
 			Tools.ToRadians(90),
 			Tools.ToRadians(0),
 			16,
 			Vector3.Zero(),
 			this.scene
 		);
-		camera.upperBetaLimit = Tools.ToRadians(80);
-		camera.lowerRadiusLimit = 11;
-		camera.upperRadiusLimit = 60;
-		const camera2 = new ArcRotateCamera(
-			'camera2',
+		boardCam.upperBetaLimit = Tools.ToRadians(80);
+		boardCam.lowerRadiusLimit = 11;
+		boardCam.upperRadiusLimit = 60;
+		boardCam.speed = 0.3;
+
+		boardCam.attachControl(true);
+		boardCam.viewport = new Viewport(0.2, 0, 0.8, 1);
+		this.scene.activeCameras?.push(boardCam);
+
+		const pocketCam = new ArcRotateCamera(
+			'pocket-cam',
 			Tools.ToRadians(90),
 			Tools.ToRadians(90),
 			10,
@@ -79,139 +81,65 @@ class App {
 			this.scene
 		);
 
-		camera.attachControl(true);
-		camera.viewport = new Viewport(0.2, 0, 0.8, 1);
-		camera2.viewport = new Viewport(0, 0, 0.2, 1);
+		pocketCam.viewport = new Viewport(0, 0, 0.2, 1);
+		this.scene.activeCameras?.push(pocketCam);
 
-		this.scene.activeCameras?.push(camera);
-		this.scene.activeCameras?.push(camera2);
-
+		// Viewport 두개로 나눠서
+		// Pointer 움직일때마다
+		// cameraToUseForPointers 변경해줘야함.
 		this.scene.onPointerObservable.add((evt) => {
 			if (evt.type == PointerEventTypes.POINTERMOVE) {
 				if (this.scene.pointerX < window.innerWidth * 0.2) {
-					this.scene.cameraToUseForPointers = camera2;
+					this.scene.cameraToUseForPointers = pocketCam;
 				} else {
-					this.scene.cameraToUseForPointers = camera;
+					this.scene.cameraToUseForPointers = boardCam;
 				}
 			}
 		});
 
-		// this.subScene.activeCamera = camera2;
+		// 키보드로 board-camera 움직이기
+		this.scene.actionManager = new ActionManager(this.scene);
 
+		this.scene.actionManager.registerAction(
+			new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, (evt) => {
+				this.inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == 'keydown';
+			})
+		);
+
+		this.scene.actionManager.registerAction(
+			new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, (evt) => {
+				this.inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == 'keydown';
+			})
+		);
+		this.scene.onBeforeRenderObservable.add(() => {
+			if (this.inputMap.w) {
+				boardCam.setPosition(boardCam.position.subtract(new Vector3(0, 0, boardCam.speed)));
+				boardCam.setTarget(boardCam.target.subtract(new Vector3(0, 0, boardCam.speed)));
+			}
+			if (this.inputMap.s) {
+				boardCam.setPosition(boardCam.position.add(new Vector3(0, 0, boardCam.speed)));
+				boardCam.setTarget(boardCam.target.add(new Vector3(0, 0, boardCam.speed)));
+			}
+			if (this.inputMap.d) {
+				boardCam.setPosition(boardCam.position.subtract(new Vector3(boardCam.speed, 0, 0)));
+				boardCam.setTarget(boardCam.target.subtract(new Vector3(boardCam.speed, 0, 0)));
+			}
+			if (this.inputMap.a) {
+				boardCam.setPosition(boardCam.position.add(new Vector3(boardCam.speed, 0, 0)));
+				boardCam.setTarget(boardCam.target.add(new Vector3(boardCam.speed, 0, 0)));
+			}
+			if (this.inputMap.o || this.inputMap[' ']) {
+				boardCam.setPosition(new Vector3(0, 15, 0));
+				boardCam.setTarget(Vector3.Zero());
+			}
+		});
+	}
+
+	private setupLight() {
 		const light = new HemisphericLight('light', new Vector3(0, 1, 0), this.scene);
 		light.intensity = 0.3;
 		const light2 = new HemisphericLight('light2', new Vector3(0, 1, 0));
-
-		Inspector.Show(this.scene, {});
-		await this.loadAssetAsync();
-		await this.scene.whenReadyAsync();
-		this.engine.hideLoadingUI();
 	}
-
-	private async loadAssetAsync() {
-		const assets = await SceneLoader.ImportMeshAsync('', './models/', 'cascadia-final.glb', this.scene);
-
-		assets.meshes.forEach((mesh, _i) => {
-			mesh.setEnabled(false);
-
-			// mesh.renderingGroupId = 1;
-			// if (mesh.id.includes('throw')) {
-			// 	Tags.AddTagsTo(mesh, 'popup');
-			// 	if (mesh.id.includes('bear')) {
-			// 		Tags.AddTagsTo(mesh, 'bear-throw');
-			// 	} else if (mesh.id.includes('elk')) {
-			// 		Tags.AddTagsTo(mesh, 'elk-throw');
-			// 	} else if (mesh.id.includes('salmon')) {
-			// 		Tags.AddTagsTo(mesh, 'salmon-throw');
-			// 	} else if (mesh.id.includes('hawk')) {
-			// 		Tags.AddTagsTo(mesh, 'hawk-throw');
-			// 	} else if (mesh.id.includes('fox')) {
-			// 		Tags.AddTagsTo(mesh, 'fox-throw');
-			// 	} else {
-			// 		Tags.AddTagsTo(mesh, 'bear-throw elk-throw salmon-throw hawk-throw fox-throw');
-			// 	}
-			// 	mesh.setEnabled(false);
-			// } else if (mesh.id.includes('wipe')) {
-			// 	Tags.AddTagsTo(mesh, 'popup');
-			// 	if (mesh.id.includes('bear')) {
-			// 		Tags.AddTagsTo(mesh, 'bear-wipe');
-			// 	} else if (mesh.id.includes('elk')) {
-			// 		Tags.AddTagsTo(mesh, 'elk-wipe');
-			// 	} else if (mesh.id.includes('salmon')) {
-			// 		Tags.AddTagsTo(mesh, 'salmon-wipe');
-			// 	} else if (mesh.id.includes('hawk')) {
-			// 		Tags.AddTagsTo(mesh, 'hawk-wipe');
-			// 	} else if (mesh.id.includes('fox')) {
-			// 		Tags.AddTagsTo(mesh, 'fox-wipe');
-			// 	} else {
-			// 		Tags.AddTagsTo(mesh, 'bear-wipe elk-wipe salmon-wipe hawk-wipe fox-wipe');
-			// 	}
-			// 	mesh.setEnabled(false);
-			// } else if (mesh.id.includes('action')) {
-			// 	Tags.AddTagsTo(mesh, 'action');
-			// 	mesh.setEnabled(false);
-			// }
-		});
-
-		// this.scene.materials.forEach((mat) => {
-		// 	console.log(mat.id);
-		// 	console.log(mat.name);
-		// });
-		// this.scene.meshes.forEach((mesh) => {
-		// 	console.log(mesh);
-		// });
-
-		// this.scene.getMeshesByTags('salmon-wipe').forEach((mesh) => {
-		// 	mesh.visibility = 1;
-		// });
-	}
-
-	// test() {
-	// 	this.scene.actionManager = new ActionManager(this.scene);
-	// 	this.scene.actionManager.registerAction(
-	// 		new ExecuteCodeAction(ActionManager.OnPickDownTrigger, async (_evt) => {})
-	// 	);
-	// }
-
-	// testMoveCam() {
-	// 	const text = this.scene.getMeshById('bear-throw')!;
-	// 	const cancel = this.scene.getMeshById('bear-throw-cancel')!;
-
-	// 	const confirm = this.scene.getMeshById('bear-throw-confirm')!;
-	// 	cancel.visibility = 1;
-	// 	confirm.visibility = 1;
-	// 	text.visibility = 1;
-	// 	const camera = this.scene.getCameraByName('camera') as ArcRotateCamera;
-	// 	const originCameraPosition = camera.position;
-	// 	const originCameraTarget = camera.target;
-
-	// 	camera.setPosition(originCameraPosition.add(new Vector3(100, 20, 100)));
-	// 	camera.setTarget(originCameraTarget.add(new Vector3(100, 0, 100)));
-
-	// 	this.scene.onPointerObservable.add((pointerInfo) => {
-	// 		if (pointerInfo.type == PointerEventTypes.POINTERDOWN) {
-	// 			const boardRay = this.scene.createPickingRay(
-	// 				this.scene.pointerX,
-	// 				this.scene.pointerY,
-	// 				Matrix.Identity(),
-	// 				this.scene.getCameraByName('camera')
-	// 			);
-	// 			const camera = this.scene.getCameraByName('camera') as ArcRotateCamera;
-
-	// 			const hitAction = this.scene.pickWithRay(boardRay, (mesh) => {
-	// 				return (
-	// 					mesh &&
-	// 					(mesh?.id == 'bear-throw-cancel' || mesh?.id == 'bear-throw-confirm')
-	// 				);
-	// 			});
-
-	// 			if (hitAction?.hit && hitAction.pickedMesh) {
-	// 				camera.setPosition(new Vector3(0, 16, 0));
-	// 				camera.setTarget(Vector3.Zero());
-	// 			}
-	// 		}
-	// 	});
-	// }
 }
 
 new App();
